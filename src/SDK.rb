fd946d35c17af06eebff2c33009b80009b87b59b
nukiFW <xavier.vdw@gmail.com>
2014-06-23 12:13:31 +0200

    try snipper

--- src/SDK.rb
+++ src/SDK.rb
@@ -229,6 +229,32 @@ class Object
                             "Renvoi un buffer (pour les Win32API's)",
                             {:size => ["Taille du buffer", :Fixnum]}, true
 
+  link_snippet("
+      #Classe qui sera un attribut
+      class A
+        attr_accessor :test
+        def initialize
+          @test = 99
+        end
+        def succ 
+          return @test + 1
+        end
+      end
+
+      #Classe qui Délègue
+      class B 
+        attr_accessor :a
+        delegate :a, :succ  #On crée la méthode Succ 
+        def initialize
+          @a = A.new
+        end
+      end
+
+      #Exemple d'usage 
+      b = B.new 
+      b.succ # 100
+    ") 
+
   #--------------------------------------------------------------------------
   # * Identity (Blank operation)
   #--------------------------------------------------------------------------
