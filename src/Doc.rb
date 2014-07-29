0d6aa9aa8d6349b77f462af466d7b58a94f4d62d
hyperaho <funkywork.courriel@gmail.com>
2014-07-29 19:05:51 +0200

    Finalize Key Doc

--- src/Doc.rb
+++ src/Doc.rb
@@ -122,13 +122,11 @@ class Point
     # * Documentation
     #--------------------------------------------------------------------------
     link_class_documentation "Représentation des touches clavier/souris"
-    link_attr_documentation :"Keys::Mouse_left", "Pointe le bouton gauche de la souris"
-    link_attr_documentation :"Keys::Mouse_right", "Pointe le bouton droit de la souris"
-    link_attr_documentation :"Keys::Mouse_center", "Pointe le bouton central de la souris"
-    link_attr_documentation :"Keys::Cancel", "Pointe la touche cancel"
-    link_attr_documentation :"Keys::Mouse_x1", "Pointe le bouton X1 de la souris"
-    link_attr_documentation :"Keys::Mouse_x2", "Pointe le bouton X2 de la souris"
-    link_attr_documentation :"Keys::Backspace", "Pointe la touche Backspace du clavier"
-    link_attr_documentation :"Keys::Tab", "Pointe la touche Tab du clavier"
+
+    All.select{|x| x!= :none}.each do |key|
+      kname = key.to_s
+      kcons = "Keys::#{kname.capitalize}"
+      link_attr_documentation kcons.to_sym, "Pointe la touche #{kname}"
+    end
 
   end
