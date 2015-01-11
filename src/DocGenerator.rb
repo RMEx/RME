309fbd8e7c653d824d0a8c0c02b1c9f7cfdca853
Jauke <joke@biloucorp.com>
2015-01-11 21:51:41 +0100

    Details2

--- src/DocGenerator.rb
+++ src/DocGenerator.rb
@@ -432,9 +432,9 @@ module DocGenerator
       # *  Save report
       #--------------------------------------------------------------------------
       def save_report(o)
-        r = "Report\n"
+        r = "Report,\n"
         r += "\# #{RME::Doc.vocab[:documented]},"
-        r += "#{Checker.documented_methods.length}/#{Command.singleton_methods.length}\n\n"
+        r += "#{Checker.documented_methods.length}/#{Command.singleton_methods.length}\n,\n"
         r += "\# #{RME::Doc.vocab[:undocumented]},\# #{RME::Doc.vocab[:suggest]}\n"
         Checker.undocumented_methods.each do |c| 
           m = RME::Doc.schema[:Command][:methods]["Command.#{c}".to_sym]
@@ -445,7 +445,7 @@ module DocGenerator
           t ||= "Aucune suggestion"
           r += "#{c},#{t}\n" 
         end
-        r += "\n\# #{RME::Doc.vocab[:orphans]},#{RME::Doc.vocab[:suggest]}\n"
+        r += ",\n\# #{RME::Doc.vocab[:orphans]},#{RME::Doc.vocab[:suggest]}\n"
         Checker.orphans.each do |c| 
           keywords = Checker.undocumented_methods
           keywords.uniq!
