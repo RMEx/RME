c699bb50bd07561c32217c94213894a2c48f305f
nukiFW <xavier.vdw@gmail.com>
2015-01-12 02:52:06 +0100

    Restreinds existant rows

--- src/DocGenerator.rb
+++ src/DocGenerator.rb
@@ -414,13 +414,10 @@ module DocGenerator
       # *  Process EE report
       #--------------------------------------------------------------------------
       def ee_report(ee)
-        g = "Command\tME\tEE\t\tRME_call\tEE_call\n"
+        g = "RM\tEE\n"
         eecmd = EE4::Command_Description.singleton_methods
         total = eecmd + Checker.commands
-        p eecmd
         total = total.uniq.sort.collect do |m|
-          a = Checker.commands.include?(m) ? "X" : "-"
-          b = eecmd.include?(m) ? "X" : "-"
           rme_call = "-"
           ee_call = "-"
           if eecmd.include?(m)
@@ -439,7 +436,7 @@ module DocGenerator
               rme_call += "(" +k.join(",")+")"
             end
           end
-          "#{m}\t#{a}\t#{b}\t\t#{rme_call}\t#{ee_call}"
+          "#{rme_call}\t#{ee_call}"
         end
         g += total.join("\n")
         FileTools.write(ee, g)
