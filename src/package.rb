62bacc5b20b367d5dd9cfc74fe48175b44330fa2
nukiFW <xavier.vdw@gmail.com>
2015-03-17 18:55:59 +0100

    Append Package feature

---
+++ src/package.rb
@@ -0,0 +1,25 @@
+# Package description
+Package.new(
+	name: 'RME',
+	version: vsn(1, 0, 0),
+	authors: {
+    'Nuki' => 'xaviervdw@gmail.com',
+    'Grim' => 'grimfw@gmail.com',
+    'Raho' => '',
+    'Hiino' => '',
+    'Joke' => 'joke@biloucorp.com',
+    'Mspawn' => ''
+  },
+	components: [
+    'SDK.rb',
+    'Database.rb',
+    'Internal.rb',
+    'EvEx.rb',
+    'Commands.rb',
+    'Incubator.rb',
+    'EE4Cmds.rb',
+    'DocGenerator.rb',
+    'Doc.rb'
+  ],
+	description: 'RME is a powerful tool to improve your RPGMaker VXAce experience!'
+)
