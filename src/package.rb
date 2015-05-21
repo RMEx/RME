862edc1297d07d15b5ab738aa9d1bb932b83c0df
BilouMaster Joke <joke@biloucorp.com>
2015-05-21 10:51:08 +0200

    Append SDK.Gui.rb

--- src/package.rb
+++ src/package.rb
@@ -1,7 +1,7 @@
 # Package description
 Package.new(
 	name: 'RME',
-	version: vsn(0, 0, 6),
+	version: vsn(0, 0, 7),
 	authors: {
     'Nuki' => 'xaviervdw@gmail.com',
     'Grim' => 'grimfw@gmail.com',
@@ -10,17 +10,18 @@ Package.new(
     'Joke' => 'joke@biloucorp.com',
     'Mspawn' => ''
   },
-	components: [
+  components: [
     'SDK.rb',
     'Database.rb',
     'Internal.rb',
+    'SDK.Gui.rb',
     'EvEx.rb',
     'Commands.rb',
     'Incubator.rb',
     'EE4Cmds.rb',
-		'Tools.rb',
+    'Tools.rb',
     'DocGenerator.rb',
     'Doc.rb'
   ],
-	description: 'RME is a powerful tool to improve your RPGMaker VXAce experience!'
+  description: 'RME is a powerful tool to improve your RPGMaker VXAce experience!'
 )
