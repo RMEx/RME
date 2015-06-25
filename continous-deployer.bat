1210b6aff9b143cbdd66079c6778a765ae3b7a95
nukiFW <xavier.vdw@gmail.com>
2015-06-26 01:39:15 +0200

    Deploy from RMEBot

---
+++ continous-deployer.bat
@@ -0,0 +1,8 @@
+START /WAIT project/Game.exe test console
+git add .
+git commit -m "Deploy from RMEBot"
+git push origin master
+cp doc/doc.js ../RMEx.github.io/RMEDoc/doc.js
+git -C ../RMEx.github.io add .
+git -C ../RMEx.github.io commit -m "Deploy from RMEBot"
+git -C ../RMEx.github.io push origin master
