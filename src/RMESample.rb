3e810c4ba7d18cfa14dcc04764c3c0858e5413e0
nukiFW <xavier.vdw@gmail.com>
2015-06-29 07:50:41 +0200

    Deploy from RMEBot

--- src/RMESample.rb
+++ src/RMESample.rb
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
 # Contient la liste des exemples
 ASample.new('Informations sur la carte', [
     'map_id', 'map_name', 'map_width', 'map_height'
   ], 9)
+
+ASample.new('Un événement à une position aléatoire', [
+    'map_id', 'random', 'id_at', 'map_width', 'map_height'
+  ], 10)
