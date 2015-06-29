bcc854caeb7d58be181182371f501a006824f409
nukiFW <xavier.vdw@gmail.com>
2015-06-29 17:17:24 +0200

    Deploy from RMEBot

--- src/RMESample.rb
+++ src/RMESample.rb
@@ -6,5 +6,8 @@ ASample.new('Informations sur la carte',
 ASample.new('Un événement à une position aléatoire',
   ['map_id', 'random', 'id_at', 'map_width', 'map_height', 'event_name'], 10)
 
-  ASample.new('Un événement dans le sable',
-    ['player_x', 'player_y', 'terrain_tag'], 11)
+ASample.new('Le joueur marchant dans le sable',
+  ['player_x', 'player_y', 'terrain_tag'], 11)
+
+ASample.new('Afficher les tiles_id de chaque couche',
+  ['player_x', 'player_y', 'tile_id', 'text_show', 'text_change'], 12)
