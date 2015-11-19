0986ba66ba8e52e1dea54407976ef9761508ffb4
Grimimi <grimfw@gmail.com>
2015-11-19 12:27:04 +0100

    fix trigger

--- src/EvEx.rb
+++ src/EvEx.rb
@@ -1405,7 +1405,7 @@ class Game_CharacterBase
   #--------------------------------------------------------------------------
   # * Eval sequence
   #--------------------------------------------------------------------------
-  def eval(str)
+  def eval(str, r=nil)
     Game_Interpreter.current_id = @id
     Game_Interpreter.current_map_id = $game_map.map_id
     script = str.gsub(/S(V|S)\[(\d+)\]/) { "S#{$1}[#{@id}, #{$2}]" }
