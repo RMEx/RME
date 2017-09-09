6a1186a983a1f01dd976c5c9f1d8a6e765fe0d65
Hinola <31566747+Hinola@users.noreply.github.com>
2017-09-09 23:39:08 +0200

    Modification de use_item et ajout de Game_Battler
    Ajout d'une mémorisation de l'item(objet, compétence) utilisé
    La valeur mémorisée est retournée par la commande last_used_item

--- src/EvEx.rb
+++ src/EvEx.rb
@@ -356,6 +356,27 @@ class Game_CommonEvent
 end
 
 #==============================================================================
+# ** Game_Battler
+#------------------------------------------------------------------------------
+#  A battler class with methods for sprites and actions added. This class 
+# is used as a super class of the Game_Actor class and Game_Enemy class.
+#==============================================================================
+
+class Game_Battler < Game_BattlerBase
+  #--------------------------------------------------------------------------
+  # * Alias
+  #--------------------------------------------------------------------------
+  alias :old_use_item :use_item
+  #--------------------------------------------------------------------------
+  # * Memorize item ID
+  #--------------------------------------------------------------------------
+  def use_item(item)
+    $game_temp.last_used_item = item.id
+    old_use_item(item)
+  end
+end
+
+#==============================================================================
 # ** Game_Troop
 #------------------------------------------------------------------------------
 #  This class handles enemy groups and battle-related data. Also performs
