5061e86083a23aef0fffe14ba46c64c24cb6d41b
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-09-18 23:33:41 +0200

    Commands to change camera lock

--- src/Commands.rb
+++ src/Commands.rb
@@ -2988,6 +2988,12 @@ module RMECommands
 
     def camera_lock; $game_map.target_camera = nil; end
     def camera_unlock; $game_map.target_camera = $game_player; end
+  
+    def camera_lock_x; $game_map.camera_lock << :x; end
+    def camera_unlock_x; $game_map.camera_lock.delete(:x); end
+      
+    def camera_lock_y; $game_map.camera_lock << :y; end
+    def camera_unlock_y; $game_map.camera_lock.delete(:y); end
 
     def camera_change_focus(event_id)
       e = event(event_id)
