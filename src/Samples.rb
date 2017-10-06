6715016df46d7c866e7b6f3fbe1d886e2486bdd4
Xavier Van de Woestyne <xavier.vdw@gmail.com>
2017-10-06 05:33:22 +0200

    Fix doc

--- src/Samples.rb
+++ src/Samples.rb
@@ -77,6 +77,6 @@ ASample.new('Choix + Message',
 ['choice', 'last_choice'], 32)
 
 ASample.new('Désactive la course quand le joueur est sur la zone 7',
-['dash_activate', 'dash_deactivate', 'dash_activate?', 'dash_deactivated?'], 33)
+['dash_activate', 'dash_deactivate', 'dash_activate?', 'dash_deactivate?'], 33)
 
 end
