a7a85567e85e9dc82688dfd5dee2df2fe050e720
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-02-19 19:25:17 +0100

    Fix typo in doc

--- src/Doc.rb
+++ src/Doc.rb
@@ -5857,7 +5857,7 @@ link_method_documentation 'Command.move_to',
 		:x => ["Coordonnées X", :Fixnum],
 		:y => ["Coordonnées Y", :Fixnum],
 		:"*wait_flag" => ["Par défaut, false, cette valeur définit le blocage durant le déplacement ou non", :Boolean],
-    :"*no_throug" => ["Par défaut, false, si cette valeur vaut true, même en mode fantôme, l'évènement utilisera un chemin passable", :Boolean],
+    :"*no_through" => ["Par défaut, false, si cette valeur vaut true, même en mode fantôme, l'évènement utilisera un chemin passable", :Boolean],
 
 	} # Maybe changed
 register_command :event, 'Command.move_to'
