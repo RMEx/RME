e005884dbf017aae2985b482da8bf1d7334255a9
Arnaud LEPESANT <arnaud.lepesant@gmail.com>
2018-04-26 19:33:12 +0200

    [#264] Initializing Map related commands refactoring

---
+++ src/commands/map.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# * RPG Maker (VX / VX.ACE) Extender
+# ------------------------------------------------------------------------------
+# {{ TODO: authors }}
+# ------------------------------------------------------------------------------
+# Maps related commands.
+#
+# [Dependencies]
+#   - `../*.rb`
+#   - `../documentation/internal_commands.rb`
+#   - `../documentation/internal_documentation.rb`
+# ==============================================================================
+
+module RME
+  module Command
+    module Map
+
+      # TODO
+      # - `map_id`
+      # - `map_name`
+      # - `id_at`
+      # - `event_at`
+      # - `terrain_tag`
+      # - `tile_id`
+      # - `set_tile_where`
+      # - `delete_tiles`
+      # - `set_tile`
+      # - `region_id`
+      # - `square_passable?`
+      # - `get_tileset_id`
+      # - `dash_activate?`
+      # - `dash_deactivate?`
+      # - `dash_activation`
+      # - `switch_tileset`
+      # - `wall?`
+      # - `roof?`
+      # - `stair?`
+      # - `table?`
+      # - `map_width`
+      # - `map_height`
+      # - `ground?`
+      # - `boat_passable?`
+      # - `ship_passable?`
+      # - `autotile_type`
+      # - `damage_floor?`
+      # - `get_random_square`
+      # - `get_squares_by_region`
+      # - `get_squares_by_tile`
+      # - `get_squares_by_terrain`
+
+    end
+  end
+end
