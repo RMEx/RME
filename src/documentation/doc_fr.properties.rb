7cd0c1f76058e3b1d52f6fce9b2008c6d3943d01
Arnaud LEPESANT <arnaud.lepesant@gmail.com>
2018-04-26 19:32:07 +0200

    [#264] Preparing environment for commands' refactoring

--- src/documentation/doc_fr.properties.rb
+++ src/documentation/doc_fr.properties.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 {
+  # Parameters' types
   'doc.Coordinate.description'      => "Coordonées d'un point dans un repère cartésien",
   'doc.Boolean.description'         => 'Valeur booléenne (deux états : vrai ou faux)',
   'doc.PositiveInteger.description' => 'Entier positif',
 
-  'doc.parameter.event.eventId.description' => "ID de l'événement à déplacer",
-  'doc.parameter.event.x.description' => 'Abscisse du point cible',
-  'doc.parameter.event.y.description' => 'Ordonnées du point cible',
-  'doc.parameter.event.ghostly.description' => "Indique si l'événement peut passer au travers de tout lorsqu'il se déplace (`true`) ou non (`false`)",
+  # Sections
+  'doc.section.Map' => "Commandes relatives aux informations des cartes"
 
-  'doc.cmd.event.move_to' => "Déplace l'événément vers le point cible"
+  # Commands
+
 }
